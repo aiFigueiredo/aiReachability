ceeef11a0a0f2fc48b5b6326df8edeb96955f0ce
Jose Miguel Figueiredo <JoseMiguel.Figueiredo@synchronoss.com>
2020-03-01 18:59:47 +0000

    Add Example project to workspace to test framework

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Jose Miguel Figueiredo on 01/03/2020.
+//  Copyright © 2020 Jose Miguel Figueiredo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Jose Miguel Figueiredo on 01/03/2020.
+//  Copyright © 2020 Jose Miguel Figueiredo. All rights reserved.
+//
+
+import UIKit
+import aiReachability
+
+class ViewController: UIViewController {
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var wifiStateLabel: UILabel!
+    @IBOutlet weak var cellularStateLabel: UILabel!
+
+    // MARK: - Private Property
+
+    private let networkMonitor = NetworkMonitor()
+
+    // MARK: - Lifecycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpLabels()
+    }
+
+    // MARK: - Private Methods
+
+    private func setUpLabels() {
+        wifiStateLabel.text = networkMonitor.wifiState.toString()
+        cellularStateLabel.text = networkMonitor.cellularState.toString()
+    }
+
+    // MARK: - IBActions
+
+    @IBAction private func updateTapped(_ sender: Any) {
+        setUpLabels()
+    }
+}
+
+extension NetworkState {
+    func toString() -> String {
+        switch self {
+        case .unknown:
+            return "Unknown"
+        case .connected:
+            return "Connected"
+        case .disconnected:
+            return "Disconnected"
+        }
+    }
+}
