c9c109774743006bf7b0b875e4c597077f2416ea
Jose Miguel Figueiredo <JoseMiguel.Figueiredo@synchronoss.com>
2020-03-01 19:17:09 +0000

    Add Package.swift file for Swift Package Manager Support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "aiReachability",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "aiReachability", targets: ["aiReachability"]),
+    ],
+    targets: [
+        .target(name: "aiReachability", dependencies: [], path: "aiReachability/aiReachability"),
+        .testTarget(name: "aiReachabilityTests", dependencies: ["aiReachability"], path: "aiReachability/aiReachabilityTests"),
+    ]
+)
